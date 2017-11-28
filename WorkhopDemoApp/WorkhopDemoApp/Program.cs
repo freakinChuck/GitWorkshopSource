bf1a21ea379bc51afbf0bd83d030b5304e3064f3
Silvio Stappung <silvio.stappung@diartis.ch>
2017-11-28 08:54:33 +0100

    Added Companion Cube for Release 2

--- WorkhopDemoApp/WorkhopDemoApp/Program.cs
+++ WorkhopDemoApp/WorkhopDemoApp/Program.cs
@@ -11,7 +11,7 @@ namespace WorkhopDemoApp
     {
         static void Main(string[] args)
         {
-            String computerNameInAppConfig = ConfigurationManager.AppSettings["MachineName"] ?? string.Empty;
+            String computerNameInAppConfig = ConfigurationManager.AppSettings["ComputerName"] ?? string.Empty;
 
             if (computerNameInAppConfig.ToUpper() == Environment.MachineName.ToUpper())
             {
@@ -21,7 +21,7 @@ namespace WorkhopDemoApp
                 Console.WriteLine("The Enrichment Center is required to remind you that you will be baked, and then there will be cake.");
 
                 //TODO: Insert Cake Write Function here ('Program.PrintCake();')
-
+                PrintCake();
                 Program.PrintFinish();
             }
             else
@@ -37,25 +37,25 @@ namespace WorkhopDemoApp
         {
             hasPrintCakeBeenCalled = true;
             string cake = @"
-	            ,:/+/-
-	            /M/              .,-=;//;-
-	       .:/= ;MH/,    ,=/+%$XH@MM#@:
-	      -$##@+$###@H@MMM#######H:.    -/H#
-	 .,H@H@ X######@ -H#####@+-     -+H###@X
-	  .,@##H;      +XM##M/,     =%@###@X;-
-	X%-  :M##########$.    .:%M###@%:
-	M##H,   +H@@@$/-.  ,;$M###@%,          -
-	M####M=,,---,.-%%H####M$:          ,+@##
-	@##################@/.         :%H##@$-
-	M###############H,         ;HM##M$=
-	#################.    .=$M##M$=
-	################H..;XM##M$=          .:+
-	M###################@%=           =+@MH%
-	@################M/.          =+H#X%=
-	=+M##############M,       -/X#X+;.
-	  .;XM##########H=    ,/X#H+:.
-	     ,:/%XM####H/.
-	          ,.:=-.
+	            ,:/+/-                                     x    ________________                  ________________
+                /M/              .,-=;//;-                 x   |                                                  |
+	       .:/= ;MH/,    ,=/+%$XH@MM#@:                    x   |                _________________                 | 
+	      -$##@+$###@H@MMM#######H:.    -/H#               x   |                                                  |
+	 .,H@H@ X######@ -H#####@+-     -+H###@X               x   |          ____                    ____            |
+	  .,@##H;      +XM##M/,     =%@###@X;-                 x   |         /                             \
+	X%-  :M##########$.    .:%M###@%:                      x   |    ____/                               \____
+	M##H,   +H@@@$/-.  ,;$M###@%,          -               x   |   |                                         |
+	M####M=,,---,.-%%H####M$:          ,+@##               x   |   |                                         |
+	@##################@/.         :%H##@$-                x   |   |                                         |
+	M###############H,         ;HM##M$=                    x   |   |                                         |
+	#################.    .=$M##M$=                        x   |   |                                         |
+	################H..;XM##M$=          .:+               x   |   |_____                               _____|
+	M###################@%=           =+@MH%               x   |         \                             /
+	@################M/.          =+H#X%=                  x   |          \____                   ____/
+	=+M##############M,       -/X#X+;.                     x   | 
+	  .;XM##########H=    ,/X#H+:.                         x   |                _________________ 
+	     ,:/%XM####H/.                                     x   |
+	          ,.:=-.                                       x   |________________                  ________________|
 ";
 
 
