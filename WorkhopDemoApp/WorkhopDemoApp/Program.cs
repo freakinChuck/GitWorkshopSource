38348ce63c175be7d6db3ae6d814e3785a75165d
Silvio Stappung <silvio.stappung@diartis.ch>
2017-11-28 08:01:42 +0100

    Initial Commit of Workhop Source

---
+++ WorkhopDemoApp/WorkhopDemoApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkhopDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String computerNameInAppConfig = ConfigurationManager.AppSettings["ComputerName"] ?? string.Empty;
+
+            if (computerNameInAppConfig.ToUpper() == Environment.MachineName.ToUpper())
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Please Adjust the Computer Name in the App.Config File for this example to work.");
+                Console.WriteLine("Press any Key to Continue....");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
